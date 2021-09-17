ac6791f24e7c4a5b3256c03aaf1dba2ea5333e26
dbaldew <dbaldew@gmail.com>
2021-09-17 03:59:29 +0200

    flexbox testground

--- src/App.js
+++ src/App.js
@@ -3,7 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Heyoo</h1>
+
+      <>
+          <div>
+              <h1>Heyoooo!!!!</h1>
+              <div className="textbox">
+                  <p>Flex direction: row</p>
+                  <p>horizontaal: justify content</p>
+                  <p>verticaal: align items</p>
+              </div>
+              <div className="textbox">
+                  <p>Flex direction: column</p>
+                  <p>verticaal: justify content</p>
+                  <p>horizontaal: align items</p>
+              </div>
+
+              <div className="container">
+                  <div className="box1">
+
+                  </div>
+              </div>
+
+
+          </div>
+          <div>
+
+          </div>
+      </>
+
+
+
+
   );
 }
 
